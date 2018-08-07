51566a2be8601ecb1dc35ca9d0eaba7e46e21b06
ucrania <ucrania131@hotmail.com>
2018-08-07 18:43:58 +0000

    test_2

--- push_code.sh
+++ push_code.sh
@@ -1,5 +1,6 @@
 git add .
 var=$(zenity --entry --text="Insert your comment, it will apear in Github!" --title="Commit comment")
-echo $var
+zenity --progress --pulsate
 git commit -m $var
 git push
+zenity --progress --percentage=100 --auto-close
